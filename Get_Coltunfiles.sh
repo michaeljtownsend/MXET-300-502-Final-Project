f438e18be9aea3d523c2292b04f30cd9b0ab9737
Michael Townsend <77943283+txmuffin@users.noreply.github.com>
2021-05-02 01:03:52 -0500

    Add files via upload
    This code will download all of the files needed using WGET.

---
+++ Get_Coltunfiles.sh
@@ -0,0 +1,12 @@
+#This file will get all of the files needed to run Coltun
+
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/L1_adc.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/L1_camera.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/L1_encoder.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/L1_motors.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/L2_kinematics.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/L2_log.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/coltun.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/go_straight.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/rotate.py
+wget https://raw.githubusercontent.com/txmuffin/MXET-300-502-Final-Project/main/start_mjpg_streamer.sh
